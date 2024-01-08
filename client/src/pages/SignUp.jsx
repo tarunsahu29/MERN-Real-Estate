416f804d3d032ae1f611a68a090704d9de88016c
Tarun Sahu <tarun929s@Taruns-MacBook-Air.local>
2024-01-09 02:05:12 +0530

    completed sign in functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -67,7 +67,7 @@ export default function SignUp() {
           disabled={loading}
           className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
         >
-          {loading ? 'Loading...' : 'Sign up'}
+          {loading ? 'Loading...' : 'Sign Up'}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
