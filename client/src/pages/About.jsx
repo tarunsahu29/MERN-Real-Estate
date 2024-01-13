428270f72e8b8d76794b767ea5d4f19b000e8312
Tarun Sahu <tarun929s@Taruns-MacBook-Air.local>
2024-01-14 01:02:39 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,8 +1,32 @@
-/* eslint-disable no-unused-vars */
-import React from 'react'
+/* eslint-disable react/no-unescaped-entities */
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Elite Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Welcome to Elite Estate, where exceptional living unfolds through unique
+        spaces telling stories of style, comfort, and possibility. Our visionary
+        team specializes in bespoke solutions for buying, selling, and renting
+        properties in vibrant neighborhoods. Every transaction at Elite Estate
+        is an artful blend of passion and precision.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Guiding you through real estate with expert advice, personalized
+        service, and a deep local market understanding. Whether you seek a
+        modern haven, strategic investment, or a cozy retreat, we turn your
+        vision into a living masterpiece.
+      </p>
+      <p className="mb-4 text-slate-700">
+        With a wealth of industry knowledge, our agents ensure each property
+        transaction is an exciting and rewarding experience. Elevate your living
+        with Elite Estate — where every transaction is a brushstroke on the
+        canvas of your dreams. Let us inspire you through a world of reimagined
+        real estate, where service is an art, advice is inspiration, and success
+        is the masterpiece we create together.
+      </p>
+    </div>
   )
 }
